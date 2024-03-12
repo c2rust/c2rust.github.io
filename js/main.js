9b658153ba731ec16fdb5a81b2dbe72f41dd98dc
HouyunCheng <ychouy@126.com>
2024-03-12 21:39:58 +0800

    Add chatglm api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const API_DEV = "";
+const API_ONLINE = "http://47.254.122.22:40311";
+
 function setNewEditor(language, selector, val='') {
     const elem = document.querySelector(selector);
 
@@ -27,7 +30,7 @@ window.editor_rust = setNewEditor('rust', '#textarea_rust', '// Here shows rust 
 
 const api = {
     transcode: async (data) => {
-        const response = await fetch('/api/transcode', {
+        const response = await fetch(API_ONLINE + '/api/transcode', {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -42,17 +45,20 @@ const api = {
 const transcode_btn = document.querySelector("#transcode_btn");
 const compile_verify_btn = document.querySelector("#compile_verify_btn");
 const sample_btn = document.querySelector('#sample_btn');
+const tool_select = document.querySelector('#tool_select');
 
 transcode_btn.clicked = false;
 transcode_btn.addEventListener('click', async () => {
+    console.log('Transcode clicked!');
     if (transcode_btn.clicked) return;
     transcode_btn.clicked = true;
 
     const c_code = window.editor_c.getValue(); 
+    const tool = tool_select.value;
+    console.log('Tool selected:', tool);
 
     const response = await api.transcode({
-        // TODO 不同的工具
-        tool: 'c2rust',
+        tool,
         code: c_code
     })
 
